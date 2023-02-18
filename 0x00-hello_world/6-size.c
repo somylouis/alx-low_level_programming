e2b9c7f45837675acbdac187ccdd142a4344dc40
root <Somylouis123@gmail.com>
2023-02-18 09:04:19 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	char charType;
 
 	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of float: %zu byte(s)\n", sizeof(flotType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 	printf("Size of double type: %zu byte(s)\n", sizeof(doubleType));
 	printf("Sizeo of char: %zu byte(s)\n", sizeof(charType));
 
