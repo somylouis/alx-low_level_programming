a7e2907bf7cad0001906a4a640a1dd33e1c8ce86
root <Somylouis123@gmail.com>
2023-02-22 13:57:47 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *@number: the integer to compute
+ * Return: the value of num
+ */
+int _abs(int number)
+{
+	if (number < 0)
+	{
+		return (-number);
+	}
+	else
+	{
+		return (number);
+	}
+}
