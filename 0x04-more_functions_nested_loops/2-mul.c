b982811af99453617c2be3fca9223f7043f1f48f
root <Somylouis123@gmail.com>
2023-02-23 13:47:19 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - int values to be multiplied
+ * @a: first value
+ * @b: second value
+ * Return: always success
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
