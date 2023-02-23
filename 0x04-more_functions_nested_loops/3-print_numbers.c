1af1dc61f0d6f059aaeecafcab60600546d36eba
root <Somylouis123@gmail.com>
2023-02-23 14:03:37 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -loopinf number from base 0
+ * Retutn: always success
+ */
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		_putchar(c + "0");
+		c++;
+	}
+	_putchar('\n');
+}
