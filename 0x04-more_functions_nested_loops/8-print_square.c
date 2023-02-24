4db6786668d0703f5760a5e8c6d56b08c14b0e4a
root <Somylouis123@gmail.com>
2023-02-24 12:48:34 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - printing the size of a square
+ * @size: number of square to be printed
+ * Return: Always success
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	}
+}
