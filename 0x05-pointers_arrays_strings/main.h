8c00211e21c8cc53d25ed509cac5f3c68582684f
root <Somylouis123@gmail.com>
2023-02-27 13:25:11 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,5 @@
-#ifndef MAIN_H
-#define MAIN.H
 
-#include <stdio.h>
-#include <unistd.h>
-
-int _putchar(char c)
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +10,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
